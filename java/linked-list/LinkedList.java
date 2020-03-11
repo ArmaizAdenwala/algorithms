2c451ed1d1e372826fb03fb93eefdc2992b9730a
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2020-03-11 02:07:14 -0700

    add linkedlist

---
+++ java/linked-list/LinkedList.java
@@ -0,0 +1,44 @@
+class LinkedList<Object> {
+  private ListNode<Object> first;
+
+  public LinkedList() {
+    this.first = null;
+  }
+
+  public void add(Object data) {
+    ListNode<Object> newNode = new ListNode(data);
+    ListNode<Object> lastNode = getLastNode(this.first);
+
+    if (lastNode == null) {
+      this.first = newNode;
+    } else {
+      lastNode.next = newNode;
+    }
+  }
+
+  public Object get(int index) {
+    ListNode<Object> node = getNodeAtIndex(this.first, index);
+    if (node == null) {
+      return null;
+    }
+    return node.data;
+  }
+
+  private ListNode<Object> getNodeAtIndex(ListNode<Object> node, int index) {
+    if (index < 0 || node == null) {
+      return null;
+    } else if (index == 0) {
+      return node;
+    }
+    return getNodeAtIndex(node.next, index - 1);
+  }
+
+  private ListNode<Object> getLastNode(ListNode<Object> node) {
+    if (node != null && node.next != null) {
+      return getLastNode(node.next);
+    }
+    return node;
+  }
+
+}
+
---
+++ java/linked-list/LinkedListMain.java
@@ -0,0 +1,13 @@
+class LinkedListMain {
+  public static void main(String[] args) {
+    LinkedList<Integer> list = new LinkedList();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    System.out.println(list.get(0));
+    System.out.println(list.get(1));
+    System.out.println(list.get(2));
+    System.out.println(list.get(3));
+    System.out.println(list.get(-1));
+  }
+}
